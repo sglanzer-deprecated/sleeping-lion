23692a477a735b91280b61a9bd5032f135e522d4
Steven Glanzer <sglanzer@accedian.com>
2018-04-01 23:38:28 -0400

    Adjusting the production deployment settings

--- config/environment.js
+++ config/environment.js
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/sleeping-lion';
+    ENV.locationType = 'hash';
     // here you can enable a production-specific feature
   }
 
